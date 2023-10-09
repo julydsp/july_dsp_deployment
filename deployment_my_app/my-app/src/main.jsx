73f4eaddfc1199c2e1c402cd290cd94f85d17873
julydsp <113667416+julydsp@users.noreply.github.com>
2023-10-09 16:08:12 +0700

    project my-app to deployment

--- deployment_my_app/my-app/src/main.jsx
+++ deployment_my_app/my-app/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
